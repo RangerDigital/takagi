49c3cda6489c12a488b98847e3b70a0e5ee7dd4f
RangerDigital <kuba07071999@gmail.com>
2020-03-29 10:33:37 +0200

    Fix Redis limiter handling

--- backend/extensions.py
+++ backend/extensions.py
@@ -10,7 +10,7 @@ from config import Configuration
 
 jwt = JWTManager()
 config = Configuration()
-limiter = Limiter(key_func=get_ipaddr, storage_uri="redis://{}:6379".format(config.REDIS_ADDRESS))
+limiter = Limiter(key_func=get_ipaddr, in_memory_fallback_enabled=True, storage_uri="redis://{}:6379".format(config.REDIS_ADDRESS))
 
 db = MongoClient(config.MONGO_ADDRESS)[config.MONGO_DATABASE]
 rd = Redis(host=config.REDIS_ADDRESS, decode_responses=True)
