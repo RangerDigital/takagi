b7f1b6f1feed29684ca821ddd7de7413917af262
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2020-03-30 22:21:18 +0200

    Add Create layout

--- frontend/src/views/Create.vue
+++ frontend/src/views/Create.vue
@@ -1,35 +1,229 @@
 <template>
-<section class="full-page">
+<section class="g-full-page">
   <NavigationBar title="Create Poll" />
-  <div class="create-flex">
-    <TextButton>CREATE</TextButton>
+  <ValidationObserver v-slot="{ invalid }">
+    <div class="g-component-flex">
+      <div>
+        <div class="form-input">
+          <p class="form-label"><img class="form-icon" src="../assets/icon-edit.svg">Poll Question</p>
+          <ValidationProvider rules="required|max:100" v-slot="v">
+            <textarea class="form-input-area" v-model="form.question" placeholder="e.g. More free time?" rows="3" cols="33"></textarea>
+            <p class="text-error" v-if="v.errors.length"><img class="form-icon" src="../assets/icon-alert-red.svg"> {{ v.errors[0] }}</p>
+          </ValidationProvider>
+        </div>
 
-  </div>
+        <div class="form-input">
+          <p class="form-label"><img class="form-icon" src="../assets/icon-tag.svg">New Option</p>
+          <ValidationProvider rules="max:50" v-slot="v">
+            <SingleInput v-model="newOption" placeholder="e.g. Yes, of course!" @onEnter="addOption" />
+            <p class="text-error" v-if="v.errors.length"><img class="form-icon" src="../assets/icon-alert-red.svg"> {{ v.errors[0] }}</p>
+          </ValidationProvider>
+
+          <TextButton @clickEvent="addOption" :disabled="!newOption[0] || invalid">ADD OPTION</TextButton>
+        </div>
+      </div>
+
+      <div class="options-list">
+        <p v-if="form.options[0]" class="form-label"><img class="form-icon" src="../assets/icon-clip.svg">Available Options {{ form.options.length }}/15)</p>
+        <p v-else class="form-label"><img class="form-icon" src="../assets/icon-note.svg">Add more options for your voters.</p>
+
+
+        <div v-for="(item, index) in form.options" :key="index">
+          <div class="options-item" @click="deleteOption(index)">
+            <p> {{ item }} </p>
+            <img class="form-icon" src="../assets/icon-x-red.svg">
+          </div>
+        </div>
+
+      </div>
+
+      <div>
+        <TextButton @clickEvent="createPoll" :disabled="invalid || !form.options[1]">CREATE</TextButton>
+        <p class="text-error"> {{ serverErrorMsg }} </p>
+        <p v-show="isSuccess" class="text-center">Success! You created a new poll!</p>
+      </div>
+    </div>
+  </ValidationObserver>
 </section>
 </template>
 
 <script>
 import NavigationBar from "../components/NavigationBar.vue";
-
 import TextButton from "../components/TextButton.vue";
+import SingleInput from "../components/SingleInput.vue";
+
+import {
+  ValidationProvider,
+  ValidationObserver,
+  extend
+} from 'vee-validate';
+
+import {
+  required,
+  min,
+  max
+} from 'vee-validate/dist/rules';
+
+extend('required', {
+  ...required,
+  message: 'This field is required!'
+});
+
+extend('min', {
+  ...min,
+  message: 'Must have at least {length} characters!'
+});
+
+extend('max', {
+  ...max,
+  message: 'Must have at most {length} characters!'
+});
 
 export default {
   name: "Create",
   components: {
     NavigationBar,
     TextButton,
+    SingleInput,
+    ValidationProvider,
+    ValidationObserver,
+  },
+  data() {
+    return {
+      form: {
+        question: '',
+        options: [],
+      },
+
+      newOption: '',
+
+      isSuccess: false,
+      serverErrorMsg: '',
+    };
+  },
+  methods: {
+    addOption() {
+      if (this.form.options.length >= 15) {
+        return;
+      }
+
+      this.form.options.push(this.newOption);
+      this.newOption = '';
+    },
+    deleteOption(index) {
+      this.form.options.splice(index, 1);
+    },
+    createPoll() {
+      console.log('Create Poll');
+    }
   }
 };
 </script>
 
 <style scoped>
-.create-flex {
-  min-height: 90%;
+.form-input {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
 
+.options-list {
+  min-height: 60%;
+}
+
+.form-label {
+  color: #626468;
+  text-align: left;
+}
+
+.vertical-divider {
+  height: 2px;
+  width: 100%;
+  border-radius: 100px;
+  background-color: #121213;
+}
+
+.options-item {
   display: flex;
-  flex-direction: column;
-
+  justify-content: space-between;
   align-items: center;
-  justify-content: space-around;
+
+  width: 30rem;
+  border-radius: 5px;
+
+  border: 1.5px solid #F0F0F0;
+  margin: 0 auto;
+
+  background-color: #FFFFFF;
+
+
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+
+  font-size: 1.6rem;
+  font-weight: 500;
+  letter-spacing: 0.1em;
+
+  color: #626468;
+  padding: 0.6em;
+}
+
+.options-item p {
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  max-width: 85%;
+}
+
+.options-item:hover {
+  border: 1.5px solid #FF7171;
+}
+
+.form-input-area {
+  width: 30rem;
+
+  display: block;
+  margin: 0 auto;
+
+  background-color: #FFFFFF;
+
+  border: 1.5px solid #121213;
+  border-radius: 5px;
+
+  margin-top: 0.5rem;
+  margin-bottom: 1rem;
+
+  font-family: 'Baloo Thambi 2';
+
+  text-align: left;
+
+  font-size: 1.6rem;
+  font-weight: 500;
+  letter-spacing: 0.1em;
+
+  color: #121213;
+  padding: 0.6em;
+}
+
+.text-center {
+  color: #626468;
+  text-align: center;
+}
+
+.text-error {
+  color: #FF7171;
+}
+
+.text-link a {
+  text-decoration: none;
+  color: #121213;
+}
+
+.form-icon {
+  display: inline;
+
+  height: 2.5rem;
+  vertical-align: text-bottom;
+
+  margin-right: 0.5rem;
 }
 </style>
