8707826e2f9c13595c79491ad22231910b2db2f6
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2020-03-31 22:48:20 +0200

    Add Sentry to frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,16 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
+
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
+
+Sentry.init({
+  dsn: 'https://64b1d0177b2e4ea2845d71b77addafb2@sentry.io/5184010',
+  integrations: [new Integrations.Vue({Vue, attachProps: true})],
+});
 
 new Vue({
   router,
