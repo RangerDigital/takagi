a2d7b4e8d6917ae5e08ad5faeb7950fd492ef172
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2020-03-31 20:57:05 +0200

    Fix Home import

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Home from "../views/Home.vue";
+
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../views/Home.vue')
+    component: Home,
   },
   {
     path: '/login',
