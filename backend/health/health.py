773859e4997f6381241705a2de154e56066bbcd1
Jakub Bednarski <kuba07071999@gmail.com>
2021-02-06 22:33:12 +0100

    Update /health

--- backend/health/health.py
+++ backend/health/health.py
@@ -33,6 +33,6 @@ def get_health():
         payload["redis_db"] = "success"
     except: # pylint: disable=W0702
         payload["redis_db"] = "failure"
-        code = 400
+        code = 503
 
     return return_json(payload), code
